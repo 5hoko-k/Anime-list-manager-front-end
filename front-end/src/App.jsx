91b119102554f5e2eea619ddcb1f99534f0938db
Shoko <shokokimera@gmail.com>
2022-08-21 11:35:31 +0300

    moved front end logic to front-end dir

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>KAKASHI</h1>
+    <>
+      <h1 className='text-5xl'>KAKASHI</h1>
+    </>
   )
 }
 
