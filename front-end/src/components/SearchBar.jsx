bf5a0f7ae6c97a7215eb9520d92f4ec3374aef45
Shoko <shokokimera@gmail.com>
2022-08-28 16:20:22 +0300

    feat: response is sorted by popularity rank before being displayed

--- front-end/src/components/SearchBar.jsx
+++ front-end/src/components/SearchBar.jsx
@@ -1,44 +1,54 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import AlgoliaSearch from './AlgoliaSearch'
+import Kitsu from 'kitsu'
 
 function SearchBar(){
 
     const [Title, setTitle] = useState([])
     const [text, setText] = useState()
+    const api = new Kitsu()
 
     const handleSearchInput = e => {
         setText(e.target.value)
     }
 
     const fetchData = () => {
-        fetch(`https://kitsu.io/api/edge/anime?&filter%5Btext%5D= ${ text }&sort=popularityRank`, {
-            Headers: { 'Accept': 'application/vnd.api+json',
-            'Context-type': 'application/vnd.api+json'}
-            })
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setTitle(data.data)
-            console.log(typeof(Title))
-            console.log(data.data)
-        })
+        // fetch(`https://kitsu.io/api/edge/anime?fields%5Battributes%5D=titles`, {
+        //     Headers: { 'Accept': 'application/vnd.api+json',
+        //     'Context-type': 'application/vnd.api+json'}
+        //     })
+        // .then(res => {
+        //     return res.json()
+        // })
+        // .then(data => {
+        //     console.log(data)
+        //     setTitle(data.data)
+        //     console.log(typeof(Title))
+        //     console.log(data.data)
+        // })
+
+        api.get('anime', { 
+                            params: { 
+                                filter: { 
+                                    text: text 
+                                }
+                            } 
+                        }
+                )
+        .then(res => { 
+            console.log(res.data)
+            setTitle(sortTheResult(res.data))
+            console.log(res.data.id)
+         })
 
         console.log("clicked")
     }
 
-    const fetchKitsuAlgoliaKeys = () => {
-        fetch('https://kitsu.io/api/edge/algolia-keys')
-        .then(res => res.json())
-        .then(data => console.log(data))
+    const sortTheResult = array => {
+        return array.sort(function (a, b) {
+            return a.popularityRank - b.popularityRank
+        })
     }
-    // useEffect(() => { 
-
-    //     const fetchData = () => {
-
-    //     }
-
-    //     setTitle(fetchData())
-    // },[])
 
     return(
         <>
@@ -48,19 +58,32 @@ function SearchBar(){
 
                 <button className="bg-green-700 hover:bg-green-600 rounded-md p-2 w-1/4 text-white" type="button" onClick={ fetchData }> Search </button>
 
-                <button className="bg-green-700 hover:bg-green-600 rounded-md p-2 w-1/4 text-white" type="button" onClick={ fetchKitsuAlgoliaKeys }> Keys </button>
+                <AlgoliaSearch />
+
             </div>
             <div>
+                <div>
+                    {Title.length > 0 && 
+                        <ul>
+                            {
+                                Title.map(anime => (
+                                    <li key={ anime.id }>{ anime.popularityRank }</li>
+                                ))
+                            }
+                        </ul>
+                    }
+                </div>
                 <div>
                     { Title.length > 0 && (
                         <div className="flex flex-wrap justify-evenly space-x-2 space-y-2">
                             { Title.map(anime => (
                                 <div key={anime.id} className="p-2 border-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
-                                    <img className="" src= { anime.attributes.posterImage.original } />
-                                    <p>{ anime.attributes.titles.en }</p>
-                                    <p>{ anime.attributes.titles.en_jp }</p>
+                                    <img className="" src= { anime.posterImage.original } />
+                                    <p>{ anime.titles.en }</p>
+                                    <p>{ anime.titles.en_jp }</p>
                                 </div>
-                            ))}
+                                ))
+                            }
                         </div>
                     )}   
                 </div>
