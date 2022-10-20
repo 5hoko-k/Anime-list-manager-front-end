2de314da6a498d4e62b545ad044dccbea8901eee
Shoko <shokokimera@gmail.com>
2022-10-20 12:04:45 +0300

    Create Fetch library component

---
+++ src/components/FetchLibrary.jsx
@@ -0,0 +1,13 @@
+function FetchLibrary(){
+
+    async function progress(res) {
+        try{
+  
+        }catch(err){
+          console.log(err)
+        }
+    }
+
+}
+
+export default FetchLibrary();
