7ac1d1dcea2d97454148f599ce599ac7ffbcc025
Shoko <shokokimera@gmail.com>
2022-10-20 13:05:39 +0300

    change export default to export (for functions) and condition of the if statements

--- src/components/FetchLibrary.jsx
+++ src/components/FetchLibrary.jsx
@@ -1,20 +1,22 @@
-function FetchLibrary(){
+export function FetchLibrary(){
     let loading = false;
     let results = null;
 
     async function progress() {
         loading = true;
+        const url = "https://anime-manager-app.herokuapp.com/";
+        // const url = "http://localhost:8000/";
         try{
-            const res = await fetch("http://localhost:8000/")
+            const res = await fetch(url)
 
-            if(res.status>200 && res.status<300){
+            if(res.status>=200 && res.status<=300){
                 results = await res.json()
                 return results;
             }else{
+                console.log(res)
                 throw new Error(res.statusText)
             }
         }catch(err){
-          console.log(err)
           return err
         }finally{
             loading = false;
@@ -24,4 +26,3 @@ function FetchLibrary(){
     return { progress }
 }
 
-export default FetchLibrary();
