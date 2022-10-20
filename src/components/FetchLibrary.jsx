0949f384befc81e613e158d498ca8fd2640c834b
Shoko <shokokimera@gmail.com>
2022-10-20 12:16:20 +0300

    Complete progress function to retch data from back-end

--- src/components/FetchLibrary.jsx
+++ src/components/FetchLibrary.jsx
@@ -1,13 +1,27 @@
 function FetchLibrary(){
+    let loading = false;
+    let results = null;
 
-    async function progress(res) {
+    async function progress() {
+        loading = true;
         try{
-  
+            const res = await fetch("http://localhost:8000/")
+
+            if(res.status>200 && res.status<300){
+                results = await res.json()
+                return results;
+            }else{
+                throw new Error(res.statusText)
+            }
         }catch(err){
           console.log(err)
+          return err
+        }finally{
+            loading = false;
         }
     }
 
+    return { progress }
 }
 
 export default FetchLibrary();
