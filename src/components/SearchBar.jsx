47b56e8aa332c6f503f3a76158f5f912572df707
Shoko <shokokimera@gmail.com>
2022-11-14 11:14:36 +0300

    change text color to material tailwind recognized colors

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -129,7 +129,7 @@ function SearchBar() {
                 <div
                   key={anime.data.id}
                   id={anime.data.id}
-                  className="p-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 text-slate-200"
+                  className="p-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 text-gray-200"
                   onClick={goToAnime}
                 >
                   <img
