3f40c5914b4f73186cbb1be20c2b1b167ff18101
Shoko <shokokimera@gmail.com>
2022-11-19 13:04:42 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <div className="w-full bg-bushGreen-shades-200 p-5">
+                The footer
+            </div>
+        </>
+    )
+}
+
+export default Footer;
