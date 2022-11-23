fdfab93c44b30a30e9413578a6c4f12de940687b
Shoko <shokokimera@gmail.com>
2022-11-23 21:08:18 +0300

    Create ErrorPage.jsx

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+function ErrorPage(){
+
+    return(
+        <>
+            <div>
+                <p>There was an error fetching the library, Please try again later</p>
+            </div>
+        </>
+    )
+}
+
+export default ErrorPage;
