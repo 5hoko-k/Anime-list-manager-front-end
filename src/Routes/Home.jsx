4f7f9778718d39dcbff415e04c7f64e45be1fc55
Shoko <shokokimera@gmail.com>
2022-11-24 12:12:26 +0300

    add search component and move search logic to search component

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Kitsu from "kitsu";
 import { FetchLibrary } from "../components/FetchLibrary"
 import LinearProgress from '@mui/material/LinearProgress';
 import Button from '@mui/material/Button'
@@ -8,55 +7,22 @@ import Footer from "../components/Footer";
 import AnimeLibray from "../components/AnimeLibrary";
 import ErrorPage from "../components/ErrorPage";
 import SearchResults from "../components/SearchResults";
+import Search from "../components/Search";
 
 function Home() {
   const [showLibrary, setShowLibrary] = useState(true);
   const [showSearchResult, setShowSearchResult] = useState(false);
-  const [searchText, setSearchText] = useState("");
   const [animeData, setAnime] = useState([]);
   const [showProgress, setShowProgress] = useState(false)
   const [showError, setShowError] = useState(false)
 
   let animeLibraryProps = null;
-  let searchResultProps = null;
 
 
-  const api = new Kitsu();
+
   const { fetchUserLibrary } = FetchLibrary();
 
   const navigate = useNavigate();
-
-  const fetchData = () => {
-    api.get("anime", {
-        params: {
-          filter: {
-            text: searchText
-           }
-        },
-      })
-      .then((res) => {
-        if (res.data){
-          searchResultProps = {"searchResults": sortTheResult(res.data)}
-          setAnime(searchResultProps);
-          setShowLibrary(false)
-          setShowError(false)
-          setShowSearchResult(true)
-        }
-
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err.message);
-      });
-
-    console.log("clicked");
-  };
-
-  const sortTheResult = (array) => {
-    return array.sort(function (a, b) {
-      return a.popularityRank - b.popularityRank;
-    });
-  };
 
   const goToAnime = (e) => {
     let animeId = e.target.id;
@@ -106,15 +72,8 @@ function Home() {
       </div>
 
       <div className="mx-auto w-3/4 mt-36 h-full">
-        <div className="flex flex-row justify-end items-center px-1 pt-10 pb-6 space-x-5">
-          <input
-            className="border-solid border-2 border-green-800 hover:border-green-900 p-1 rounded-md"
-            type="search"
-            placeholder="Search"
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <Button variant='contained' color='success' onClick={fetchData}> Search </Button>
-        </div>
+
+        <Search libraryTrigger={setShowLibrary} searchResultTrigger={setShowSearchResult} errorTrigger={setShowError} setAnimeList={setAnime}/>
 
         <div className="flex mx-auto h-full p-5 bg-bushGreen-shades-500 border border-green-600">
           {showLibrary && <AnimeLibray props={animeData} goToAnime={goToAnime} />}
