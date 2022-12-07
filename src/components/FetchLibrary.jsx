dcda9d925d1e75a779957b5ef9b71036c12dcc3b
Shoko <shokokimera@gmail.com>
2022-12-07 11:34:55 +0300

    add fetch library data post request

--- src/components/FetchLibrary.jsx
+++ src/components/FetchLibrary.jsx
@@ -20,6 +20,30 @@ export function FetchLibrary(){
         }
     }
 
-    return { fetchUserLibrary }
+    async function fetchLibraryPage(url) {
+        try{
+            const res = await fetch(url, { 
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "url": pageLinks.next })
+              })
+
+            if(res.status>=200 && res.status<=300){
+                results = await response.json()
+                console.log(results)
+                return results;
+            }else{
+                console.log(res)
+                throw new Error(res.statusText)
+            }
+        }catch(err){
+
+          return err
+        }
+    }
+
+    return { fetchUserLibrary, fetchLibraryPage }
 }
 
